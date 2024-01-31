a2b5b15a8a145df36f33aececc521fcb044af93e
Divyansh Bhushan <divyanshbhushan.business@gmail.com>
2024-01-31 23:32:11 +0530

    Update CSS and JS files.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,21 @@
-const formContainer = document.querySelector('#form_container');
-const formModalTriggerBtn = document.querySelector('#formModalTriggerBtn');
 
-formModalTriggerBtn.addEventListener("click", () =>{
-    formContainer.classList.toggle('hidden')
-})
+const inputAnimation = ()=>{const inputAnimArr = document.querySelectorAll('.input-anim')
+inputAnimArr.forEach((e)=>{
+    const inputBar = document.querySelectorAll('input')
+    const toStyle = `font-size: 0.75rem; top: -.5rem; line-height: 1rem;`
+    const fromStyle = `top: .5rem; font-size: 1rem; line-height: 1.5rem;`
+    inputBar.forEach((f)=>{
+        if(f.value === ""){
+            e.style = fromStyle
+            setInterval(() => {
+                if(f.value !== ""){
+                    e.style = toStyle
+                }
+                else{
+                    e.style = fromStyle
+                }
+            }, 1);
+        }
+    })
+})}
+inputAnimation()

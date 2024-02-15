f322ae56ee6e5afc6109a0d1b54bc30d20f31f4b
Divyansh Bhushan <divyanshbhushan.business@gmail.com>
2024-02-15 18:29:51 +0530

    Expense dashboard design updated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,11 +7,9 @@ const addNewExpense = () => {
     addNewExpenseToggler.addEventListener("click", () => {
       if(addNewExpenseContainer.style.scale === "1"){
         addNewExpenseContainer.style.scale = 0;
-        addNewExpenseTogglers[0].style.display = "block";
       } else{
         addNewExpenseContainer.classList.remove('scale-[0]')
         addNewExpenseContainer.style.scale = 1;
-        addNewExpenseTogglers[0].style.display = "none";
       }
     })
   })
